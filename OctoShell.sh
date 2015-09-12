8b3608f3100213505139109aa9ce205e53250b70
Karel Fiala <fiala.karel@gmail.com>
2015-09-12 20:07:18 +0200

    code cleanup -- now use jq for json parsing

--- OctoShell.sh
+++ OctoShell.sh
@@ -1,71 +1,81 @@
 #!/bin/bash
 
+# ***** *****  controls ***** *****
 
+# check arguments
 if [ $# -lt 1  ]
 then
 	printf "\n\n./OctoShell.sh (status|upload *.gcode|uploadprint *.gcode)\n\n"
 	exit 1
 fi
 
+# load config
 source $(dirname "${BASH_SOURCE[0]}")/printer.cfg
 
-if [ $1 == "status" ]
-then
+# ***** *****  functions ***** *****
 
-x=$(curl -s -H "X-Api-Key: $api_key" $printer/printer | grep -E "(text|actual|target)" | cut -d":" -f 2 | sed "s/\"//g;s/,//g;s/ //g")
+function do_status() {
 
-state=$(echo "$x" | head -n 1)
+        # query printer
+        q1=$(curl -s -H "X-Api-Key: $api_key" $printer/printer)
 
-bed1=$(echo "$x" | head -n 2 | tail -n 1)
-bed2=$(echo "$x" | head -n 3 | tail -n 1)
+        # parse arguments
+        state=$(echo "$q1" | jq -r '.state.text')
+        bed_actual=$(echo "$q1" | jq -r '.temperature.bed.actual')
+        bed_target=$(echo "$q1" | jq -r '.temperature.bed.target')
+        he_actual=$(echo "$q1" | jq -r '.temperature.tool0.actual')
+        he_target=$(echo "$q1" | jq -r '.temperature.tool0.target')
+        
+        # query job
+        q2=$(curl -s -H "X-Api-Key: $api_key" $printer/job)
 
-he1=$(echo "$x" | head -n 4 | tail -n 1)
-he2=$(echo "$x" | head -n 5 | tail -n 1)
+        # parse arguments
+        comp=$(echo "$q2" |  jq -r '.progress.completion' )
+        time=$(echo "$q2" | jq -r '.progress.printTime' )
+        time_left=$(echo "$q2" | jq -r '.progress.printTimeLeft')
 
-
-x=$(curl -s -H "X-Api-Key: $api_key" $printer/job | grep -E "(completion|printTime|printTimeLest)" | cut -d":" -f 2 | sed "s/\"//g;s/,//g;s/ //g")
-
-comp=$(echo "$x" | head -n 1 )
-time=$(echo "$x" | head -n 2 | tail -n 1 )
-time_left=$(echo "$x" | head -n 3 | tail -n 1 )
-
-if [ $comp == "null" ]
-then
-	comp=""
-else
-	comp="$comp %%"
-fi
-
-if [ $time == "null" ]
-then
-	time=0
-fi
-
-if [ $time_left == "null" ]
-then
-	time_left=0
-fi
+        # NULL substitution
+        if [ $comp == "null" ]; then comp="" ; else comp="$comp %%" ; fi
+        if [ $time == "null" ]; then time=0 ; fi
+        if [ $time_left == "null" ]; then time_left=0 ; fi
 
 
-printf "\n Time: `date +"%H:%M:%S"`\nState: $state $comp\n  Bed:  $bed1 /  $bed2\n   HE: $he1 / $he2\n Time: `date -d@$time -u +%H:%M:%S` / `date -d@$time_left -u +%H:%M:%S`\n"
+        printf "\tTime:\t`date +"%H:%M:%S"`\n"
+        printf "\tState:\t$state $comp\n"
+        printf "\tBed:\t$bed_actual /  $bed_target\n"
+        printf "\tHE:\t$he_actual / $he_target\n"
+        printf "\tTime:\t`date -d@$time -u +%H:%M:%S` / `date -d@$time_left -u +%H:%M:%S`\n"
 
-fi
+        
+}
 
-
-if [ $1 == "upload" ]
-then
+function do_upload() {
 	shift
 	for f in "$@"
 	do
 		curl -H "X-Api-Key: $api_key" -F select=false -F print=false -F file=@$f "$printer/files/local" 
 	done
-fi
+}
 
-if [ $1 == "uploadprint" ]
-then
+function do_uploadprint() {
 	shift
 	curl -H "X-Api-Key: $api_key" -F select=true -F print=true -F file=@$1 "$printer/files/local" 
-fi
+}
 
+
+# ***** *****  main ***** *****
+
+if [ $1 == "status" ]
+then
+    do_status
+    
+elif [ $1 == "upload" ]
+then
+    do_upload "$@"
+    
+elif [ $1 == "uploadprint" ]
+then
+    do_uploadprint "$@"
+fi
 
 exit 0
